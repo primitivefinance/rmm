1fca247cb9b1ec4e91ab4b1fab259ac7b78903b1
Kinrezc <matt.czernik@gmail.com>
2024-05-10 16:19:30 -0400

    deployment scripts w factory and live pendle wsteth pool

--- script/DeployPool.s.sol
+++ script/DeployPool.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {Script, console2} from "forge-std/Script.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {PendleERC20SY} from "pendle/core/StandardizedYield/implementations/PendleERC20SY.sol";
+import {Factory} from "../src/Factory.sol";
 import {SYBase} from "pendle/core/StandardizedYield/SYBase.sol";
 import {IStandardizedYield} from "pendle/interfaces/IStandardizedYield.sol";
 import {PendleYieldContractFactoryV2} from "pendle/core/YieldContractsV2/PendleYieldContractFactoryV2.sol";
@@ -14,67 +15,98 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 import "pendle/core/Market/MarketMathCore.sol";
 import "pendle/interfaces/IPAllActionV3.sol";
 import {IPPrincipalToken} from "pendle/interfaces/IPPrincipalToken.sol";
+import {IPMarket} from "pendle/interfaces/IPMarket.sol";
 import {IStandardizedYield} from "pendle/interfaces/IStandardizedYield.sol";
 import {IPYieldToken} from "pendle/interfaces/IPYieldToken.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
 contract DeployPool is Script {
+    using MarketMathCore for MarketState;
+    using MarketMathCore for int256;
+    using MarketMathCore for uint256;
+    using FixedPointMathLib for uint256;
     using PYIndexLib for IPYieldToken;
     using PYIndexLib for PYIndex;
-
-    function setUp() public {}
 
     string public constant ENV_PRIVATE_KEY = "PRIVATE_KEY";
     address payable public constant RMM_ADDRESS = payable(address(0));
     address public constant SY_ADDRESS = address(0);
     address public constant PT_ADDRESS = address(0);
-    address public constant YT_ADDRESS = address(0);
-    uint256 public constant startPrice = 1 ether;
-    uint256 public constant initialDepositX = 1 ether;
-    uint256 public constant strike = 1 ether;
-    uint256 public constant sigma = 1 ether;
-    uint256 public constant tau = 365 days;
-    uint256 public constant fee = 0;
+    uint256 public constant fee = 0.0002 ether;
     address public constant curator = address(0);
+    Factory FACTORY = Factory(0x519172BB1f45A5420090f03bAFd19A76AC9bC772);
+    IPMarket market = IPMarket(0xC374f7eC85F8C7DE3207a10bB1978bA104bdA3B2);
+    IPAllActionV3 router = IPAllActionV3(0x00000000005BBB0EF59571E58418F9a4357b68A0);
+    address wstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0; //real wsteth
+    RMM public rmm;
+
+    IStandardizedYield public SY;
+    IPPrincipalToken public PT;
+    IPYieldToken public YT;
+    MarketState public mktState;
+    int256 rateAnchor;
+    int256 rateScalar;
+    uint256 timeToExpiry;
+
+    address sender;
+
+    function setUp() public {
+        (SY, PT, YT) = IPMarket(market).readTokens();
+        mktState = market.readState(address(router));
+        console2.log("expiry", mktState.expiry);
+        console2.log("timestamp", block.timestamp);
+        timeToExpiry = mktState.expiry - block.timestamp;
+        rateScalar = mktState._getRateScalar(timeToExpiry);
+        rateAnchor =
+            mktState.totalPt._getRateAnchor(mktState.lastLnImpliedRate, mktState.totalSy, rateScalar, timeToExpiry);
+    }
+
+    function getPtExchangeRate() internal view returns (int256) {
+        return mktState.totalPt._getExchangeRate(mktState.totalSy, rateScalar, rateAnchor, 0);
+    }
+
+    function mintSY(uint256 amount) public {
+        console2.log("balance wsteth", IERC20(wstETH).balanceOf(sender));
+        IERC20(wstETH).approve(address(SY), type(uint256).max);
+        SY.deposit(sender, address(wstETH), amount, 1);
+    }
+
+    function mintPtYt(uint256 amount) public {
+        SY.transfer(address(YT), amount);
+        YT.mintPY(sender, sender);
+    }
 
     function run() public {
         uint256 pk = vm.envUint(ENV_PRIVATE_KEY);
-        address sender = vm.addr(pk);
         vm.startBroadcast(pk);
+        sender = vm.addr(pk);
+        rmm = FACTORY.createRMM("Lido Staked ETH 24 Dec 2025", "stETH-24DEC25");
+        console2.log("rmm address: ", address(rmm));
 
-        require(RMM_ADDRESS != address(0), "RMM_ADDRESS not set");
-        require(SY_ADDRESS != address(0), "SY_ADDRESS not set");
-        require(PT_ADDRESS != address(0), "PT_ADDRESS not set");
+        mintSY(10_000 ether);
+        mintPtYt(5_000 ether);
 
-        uint256 maturity = tau + block.timestamp;
+        IERC20(wstETH).approve(address(rmm), type(uint256).max);
+        IERC20(SY).approve(address(rmm), type(uint256).max);
+        IERC20(PT).approve(address(rmm), type(uint256).max);
+        IERC20(YT).approve(address(rmm), type(uint256).max);
 
-        (uint256 initialLiquidity, uint256 initialDepositY) = RMM(RMM_ADDRESS).prepareInit({
-            priceX: startPrice,
-            totalAsset: initialDepositX,
-            strike_: strike,
-            sigma_: sigma,
-            maturity_: maturity
-        });
+        IERC20(wstETH).approve(address(router), type(uint256).max);
+        IERC20(SY).approve(address(router), type(uint256).max);
+        IERC20(PT).approve(address(router), type(uint256).max);
+        IERC20(YT).approve(address(router), type(uint256).max);
+        IERC20(market).approve(address(router), type(uint256).max);
 
-        if (ERC20(SY_ADDRESS).allowance(msg.sender, address(this)) < initialDepositX) {
-            ERC20(SY_ADDRESS).approve(RMM_ADDRESS, initialDepositX);
-        }
-
-        if (ERC20(PT_ADDRESS).allowance(msg.sender, address(this)) < initialDepositY) {
-            ERC20(PT_ADDRESS).approve(RMM_ADDRESS, initialDepositY);
-        }
-
-        RMM(RMM_ADDRESS).init({
-            PT_: PT_ADDRESS,
-            priceX: startPrice,
-            amountX: initialDepositX,
-            strike_: strike,
-            sigma_: sigma,
+        uint256 price = uint256(getPtExchangeRate());
+        rmm.init({
+            PT_: address(PT),
+            priceX: price,
+            amountX: uint256(mktState.totalSy),
+            strike_: uint256(rateAnchor),
+            sigma_: 0.02 ether,
             fee_: fee,
-            curator_: curator
+            curator_: address(0x55)
         });
-
-        uint256 balance = ERC20(RMM_ADDRESS).balanceOf(sender);
-        console2.log("RMM LPT balance: ", balance);
 
         vm.stopBroadcast();
     }
