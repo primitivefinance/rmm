7dfad13e8cdbad51d2397cbb8e5b3f2f131f24ef
clemlak <clementlakhal@gmail.com>
2024-05-14 15:55:24 +0200

    test: add SetUp file

---
+++ test/SetUp.sol
@@ -0,0 +1,13 @@
+/// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {RMM} from "./../src/RMM.sol";
+
+contract SetUp is Test {
+    RMM public rmm;
+
+    address public wstETH;
+
+    function setUp() public {}
+}
