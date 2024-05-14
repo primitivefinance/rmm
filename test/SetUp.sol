52a341c05d02444523c2df6ba104f4f865ef1456
clemlak <clementlakhal@gmail.com>
2024-05-14 16:00:41 +0200

    test: deploy WETH in tests

--- test/SetUp.sol
+++ test/SetUp.sol
@@ -2,14 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import {WETH} from "solmate/tokens/WETH.sol";
 import {RMM} from "./../src/RMM.sol";
 
 contract SetUp is Test {
     RMM public rmm;
+    WETH public weth;
 
     address public wstETH;
 
     function setUp() public {
-        rmm = new RMM(address(0), "Test", "TST");
+        weth = new WETH();
+        rmm = new RMM(address(weth), "Test", "TST");
     }
 }
