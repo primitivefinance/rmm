82aec0bebd673d8c7437b8d8cdaa8310c8a3fa16
clemlak <clementlakhal@gmail.com>
2024-05-14 16:28:11 +0200

    test: update SetUp test file

--- test/SetUp.sol
+++ test/SetUp.sol
@@ -4,15 +4,21 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import {WETH} from "solmate/tokens/WETH.sol";
 import {RMM} from "./../src/RMM.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {PendleERC20SY} from "pendle/core/StandardizedYield/implementations/PendleERC20SY.sol";
 
 contract SetUp is Test {
     RMM public rmm;
     WETH public weth;
+    PendleERC20SY public syToken;
+    MockERC20 public yieldToken;
 
     address public wstETH;
 
     function setUp() public {
         weth = new WETH();
         rmm = new RMM(address(weth), "Test", "TST");
+        yieldToken = new MockERC20("YieldToken", "YLD", 18);
+        syToken = new PendleERC20SY("SYToken", "SY", address(yieldToken));
     }
 }
