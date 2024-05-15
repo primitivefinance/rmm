818477e047274b075218e2be26d45c0158cfa202
clemlak <clementlakhal@gmail.com>
2024-05-15 16:23:32 +0200

    test: add Deallocate test file

---
+++ test/Deallocate.t.sol
@@ -0,0 +1,30 @@
+/// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {SetUp} from "./SetUp.sol";
+
+contract DeallocateTest is SetUp {
+    function test_deellocate_BurnsLiquidity() public {
+        vm.skip(true);
+    }
+
+    function test_deallocate_AdjustsPool() public {
+        vm.skip(true);
+    }
+
+    function test_deallocate_TransfersTokens() public {
+        vm.skip(true);
+    }
+
+    function test_deallocate_EmitsDeallocate() public {
+        vm.skip(true);
+    }
+
+    function test_deallocate_RevertsIfInsufficientSYOutput() public {
+        vm.skip(true);
+    }
+
+    function test_deallocate_RevertsIfInsufficientPTOutput() public {
+        vm.skip(true);
+    }
+}
