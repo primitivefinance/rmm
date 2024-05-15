1e9beef891f77ff9c2aca3a729cb3f7ef3b2bdcd
clemlak <clementlakhal@gmail.com>
2024-05-15 16:21:44 +0200

    test: add Allocate test file

---
+++ test/Allocate.t.sol
@@ -0,0 +1,26 @@
+/// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {SetUp} from "./SetUp.sol";
+
+contract AllocateTest is SetUp {
+    function test_allocate_MintsLiquidity() public {
+        vm.skip(true);
+    }
+
+    function test_allocate_AdjustsPool() public {
+        vm.skip(true);
+    }
+
+    function test_allocate_TransfersTokens() public {
+        vm.skip(true);
+    }
+
+    function test_allocate_EmitsAllocate() public {
+        vm.skip(true);
+    }
+
+    function test_allocate_RevertsIfInsufficientLiquidityOut() public {
+        vm.skip(true);
+    }
+}
