e8c9fe30ec4635b3644121ed10af87229fa051c4
clemlak <clementlakhal@gmail.com>
2024-05-15 16:30:25 +0200

    test: add public debit and credit functions to MockRMM

--- test/MockRMM.sol
+++ test/MockRMM.sol
@@ -7,4 +7,12 @@ contract MockRMM is RMM {
     constructor(address weth_, string memory name_, string memory symbol_) RMM(weth_, name_, symbol_) {
         WETH = weth_;
     }
+
+    function debit(address token, uint256 amountWad, bytes memory data) internal returns (uint256 paymentNative) {
+        return _debit(token, amountWad, data);
+    }
+
+    function credit(address token, address to, uint256 amount) internal returns (uint256 paymentNative) {
+        return _credit(token, to, amount);
+    }
 }
