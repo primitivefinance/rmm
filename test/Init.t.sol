71be8cd4fdfb62f7db0ed1a5fc523546043060d0
clemlak <clementlakhal@gmail.com>
2024-05-15 15:52:47 +0200

    test: add test_init_StoresInitParams

--- test/Init.t.sol
+++ test/Init.t.sol
@@ -21,22 +21,18 @@ contract InitTest is SetUp {
         _;
     }
 
-    function test_init_works() public {
+    function test_init_StoresInitParams() public {
         InitParams memory initParams = InitParams({
             priceX: 1 ether,
             totalAsset: 1 ether,
             strike: 1 ether,
             sigma: 0.015 ether,
-            maturity: block.timestamp + 180 days,
+            maturity: PT.expiry(),
             PT: address(PT),
             amountX: 1 ether,
             fee: 0.00016 ether,
             curator: address(0x55)
         });
-
-        (uint256 totalLiquidity, uint256 amountY) = rmm.prepareInit(
-            initParams.priceX, initParams.totalAsset, initParams.strike, initParams.sigma, initParams.maturity
-        );
 
         rmm.init(
             initParams.PT,
@@ -47,6 +43,12 @@ contract InitTest is SetUp {
             initParams.fee,
             initParams.curator
         );
+
+        assertEq(rmm.strike(), initParams.strike);
+        assertEq(rmm.sigma(), initParams.sigma);
+        assertEq(rmm.fee(), initParams.fee);
+        assertEq(rmm.maturity(), initParams.maturity);
+        assertEq(rmm.curator(), initParams.curator);
     }
 
     function test_init_StoresTokens() public initDefaultPool {
