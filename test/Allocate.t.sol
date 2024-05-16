d174fb2081505e712b8bba22a2fca4b8ab0007da
clemlak <clementlakhal@gmail.com>
2024-05-16 20:44:35 +0200

    test: add test_allocate_MintsLiquidity

--- test/Allocate.t.sol
+++ test/Allocate.t.sol
@@ -1,11 +1,28 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {PYIndex} from "./../src/RMM.sol";
 import {SetUp} from "./SetUp.sol";
 
 contract AllocateTest is SetUp {
-    function test_allocate_MintsLiquidity() public {
-        vm.skip(true);
+    struct InitParams {
+        uint256 priceX;
+        uint256 totalAsset;
+        uint256 strike;
+        uint256 sigma;
+        uint256 maturity;
+        address PT;
+        uint256 amountX;
+        uint256 fee;
+        address curator;
+    }
+
+    function test_allocate_MintsLiquidity() public initDefaultPool {
+        deal(address(SY), address(this), 1_000 ether);
+
+        (uint256 deltaXWad, uint256 deltaYWad,,) =
+            rmm.prepareAllocate(0.1 ether, 0.1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
+        rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
     }
 
     function test_allocate_AdjustsPool() public {
