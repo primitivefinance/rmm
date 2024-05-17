4978a25849d2ca7e9da02e55bd11716027d7e482
clemlak <clementlakhal@gmail.com>
2024-05-17 14:29:12 +0200

    test: add imports for later in Debit tests

--- test/Debit.t.sol
+++ test/Debit.t.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 import {RMM} from "./SetUp.sol";
 
+// These imports will be used for extra tests
+import {FeeOnTransferToken} from "../src/test/FeeOnTransferToken.sol";
+import {ReturnsTooLittleToken} from "solmate/test/utils/weird-tokens/ReturnsTooLittleToken.sol";
+import {ReturnsTooMuchToken} from "solmate/test/utils/weird-tokens/ReturnsTooMuchToken.sol";
+import {MissingReturnToken} from "solmate/test/utils/weird-tokens/MissingReturnToken.sol";
+import {ReturnsFalseToken} from "solmate/test/utils/weird-tokens/ReturnsFalseToken.sol";
+
 contract DebitTest is Test {}
