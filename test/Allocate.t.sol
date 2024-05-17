ddfd1b312ceef9ea78cc1efc7c44089fb66a843b
clemlak <clementlakhal@gmail.com>
2024-05-17 13:43:32 +0200

    test: update test_allocate_MintsLiquidity

--- test/Allocate.t.sol
+++ test/Allocate.t.sol
@@ -10,11 +10,16 @@ contract AllocateTest is SetUp {
 
         (uint256 deltaXWad, uint256 deltaYWad,,) =
             rmm.prepareAllocate(0.1 ether, 0.1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
-        rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
+
+        uint256 preTotalLiquidity = rmm.totalLiquidity();
+        uint256 deltaLiquidity = rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
+        assertEq(rmm.totalLiquidity(), preTotalLiquidity + deltaLiquidity, "wrong total liquidity");
     }
 
-    function test_allocate_AdjustsPool() public {
-        vm.skip(true);
+    function test_allocate_AdjustsPool() public initDefaultPool {
+        (uint256 deltaXWad, uint256 deltaYWad,,) =
+            rmm.prepareAllocate(0.1 ether, 0.1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
+        rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
     }
 
     function test_allocate_TransfersTokens() public {
