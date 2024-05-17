ec4ad2c292fcdffd1a7efb0f7180f20b3d4960aa
clemlak <clementlakhal@gmail.com>
2024-05-17 16:05:55 +0200

    test: add test_deallocate_TransfersTokens

--- test/Deallocate.t.sol
+++ test/Deallocate.t.sol
@@ -37,8 +37,24 @@ contract DeallocateTest is SetUp {
         assertEq(rmm.lastTimestamp(), block.timestamp);
     }
 
-    function test_deallocate_TransfersTokens() public {
-        vm.skip(true);
+    function test_deallocate_TransfersTokens() public initDefaultPool dealSY(address(this), 1_000 ether) {
+        (uint256 deltaXWad, uint256 deltaYWad,,) =
+            rmm.prepareAllocate(0.1 ether, 0.1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
+        (uint256 deltaLiquidity) = rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
+        uint256 lptBurned;
+
+        uint256 thisPreBalanceSY = SY.balanceOf(address(this));
+        uint256 thisPreBalancePT = PT.balanceOf(address(this));
+        uint256 rmmPreBalanceSY = SY.balanceOf(address(rmm));
+        uint256 rmmPreBalancePT = PT.balanceOf(address(rmm));
+
+        (deltaXWad, deltaYWad, lptBurned) = rmm.prepareDeallocate(deltaLiquidity / 2);
+        rmm.deallocate(deltaLiquidity / 2, 0, 0, address(this));
+
+        assertEq(SY.balanceOf(address(this)), thisPreBalanceSY + deltaXWad);
+        assertEq(PT.balanceOf(address(this)), thisPreBalancePT + deltaYWad);
+        assertEq(SY.balanceOf(address(rmm)), rmmPreBalanceSY - deltaXWad);
+        assertEq(PT.balanceOf(address(rmm)), rmmPreBalancePT - deltaYWad);
     }
 
     function test_deallocate_EmitsDeallocate() public {
