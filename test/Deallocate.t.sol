f839f215c55e2e9e2b8dbc09a51d91fdf755f6e0
clemlak <clementlakhal@gmail.com>
2024-05-17 16:23:55 +0200

    test: add test_deallocate_RevertsIfInsufficientSYOutput

--- test/Deallocate.t.sol
+++ test/Deallocate.t.sol
@@ -64,18 +64,34 @@ contract DeallocateTest is SetUp {
         uint256 lptBurned;
 
         (deltaXWad, deltaYWad, lptBurned) = rmm.prepareDeallocate(deltaLiquidity / 2);
-        rmm.deallocate(deltaLiquidity / 2, 0, 0, address(this));
 
         vm.expectEmit(true, true, true, true);
         emit RMM.Deallocate(address(this), address(this), deltaXWad, deltaYWad, deltaLiquidity / 2);
         rmm.deallocate(deltaLiquidity / 2, 0, 0, address(this));
     }
 
-    function test_deallocate_RevertsIfInsufficientSYOutput() public {
-        vm.skip(true);
+    function test_deallocate_RevertsIfInsufficientSYOutput()
+        public
+        initDefaultPool
+        dealSY(address(this), 1_000 ether)
+    {
+        (uint256 deltaXWad, uint256 deltaYWad,,) =
+            rmm.prepareAllocate(0.1 ether, 0.1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
+        (uint256 deltaLiquidity) = rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
+        uint256 lptBurned;
+
+        (deltaXWad, deltaYWad, lptBurned) = rmm.prepareDeallocate(deltaLiquidity / 2);
+        vm.expectRevert(
+            abi.encodeWithSelector(RMM.InsufficientOutput.selector, deltaLiquidity / 2, deltaXWad + 1, deltaXWad)
+        );
+        rmm.deallocate(deltaLiquidity / 2, deltaXWad + 1, 0, address(this));
     }
 
-    function test_deallocate_RevertsIfInsufficientPTOutput() public {
+    function test_deallocate_RevertsIfInsufficientPTOutput()
+        public
+        initDefaultPool
+        dealSY(address(this), 1_000 ether)
+    {
         vm.skip(true);
     }
 }
