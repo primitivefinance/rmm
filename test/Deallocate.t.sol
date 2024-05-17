a267201f028441bd1e6ba99c2fd3a4c9f6d3f303
clemlak <clementlakhal@gmail.com>
2024-05-17 14:29:20 +0200

    test: add test_deellocate_BurnsLiquidity test

--- test/Deallocate.t.sol
+++ test/Deallocate.t.sol
@@ -1,11 +1,19 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {SetUp} from "./SetUp.sol";
+import {PYIndex} from "./../src/RMM.sol";
+import {SetUp, RMM} from "./SetUp.sol";
 
 contract DeallocateTest is SetUp {
-    function test_deellocate_BurnsLiquidity() public {
-        vm.skip(true);
+    function test_deellocate_BurnsLiquidity() public initDefaultPool dealSY(address(this), 1_000 ether) {
+        (uint256 deltaXWad, uint256 deltaYWad,,) =
+            rmm.prepareAllocate(0.1 ether, 0.1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
+
+        (uint256 deltaLiquidity) = rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
+
+        uint256 lptBurned;
+        (deltaXWad, deltaYWad, lptBurned) = rmm.prepareDeallocate(deltaLiquidity / 2);
+        rmm.deallocate(deltaLiquidity / 2, 0, 0, address(this));
     }
 
     function test_deallocate_AdjustsPool() public {
