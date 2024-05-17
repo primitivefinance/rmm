138717151f66e7310e2370584547063a89b81d1e
clemlak <clementlakhal@gmail.com>
2024-05-17 13:51:47 +0200

    test: add test_allocate_EmitsAllocate

--- test/Allocate.t.sol
+++ test/Allocate.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {PYIndex} from "./../src/RMM.sol";
-import {SetUp} from "./SetUp.sol";
+import {SetUp, RMM} from "./SetUp.sol";
 
 contract AllocateTest is SetUp {
     function test_allocate_MintsLiquidity() public initDefaultPool {
@@ -49,8 +49,16 @@ contract AllocateTest is SetUp {
         assertEq(PT.balanceOf(address(rmm)), rmmPreBalancePT + deltaYWad);
     }
 
-    function test_allocate_EmitsAllocate() public {
-        vm.skip(true);
+    function test_allocate_EmitsAllocate() public initDefaultPool {
+        deal(address(SY), address(this), 1_000 ether);
+
+        (uint256 deltaXWad, uint256 deltaYWad, uint256 deltaLiquidity,) =
+            rmm.prepareAllocate(0.1 ether, 0.1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
+
+        vm.expectEmit(true, true, true, true);
+
+        emit RMM.Allocate(address(this), address(this), deltaXWad, deltaYWad, deltaLiquidity);
+        rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
     }
 
     function test_allocate_RevertsIfInsufficientLiquidityOut() public {
