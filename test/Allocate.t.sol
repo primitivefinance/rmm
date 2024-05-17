333773ccd3dba440e9ddfcf90d25d5a856617eef
clemlak <clementlakhal@gmail.com>
2024-05-17 13:47:30 +0200

    test: add test_allocate_AdjustsPool

--- test/Allocate.t.sol
+++ test/Allocate.t.sol
@@ -13,13 +13,22 @@ contract AllocateTest is SetUp {
 
         uint256 preTotalLiquidity = rmm.totalLiquidity();
         uint256 deltaLiquidity = rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
-        assertEq(rmm.totalLiquidity(), preTotalLiquidity + deltaLiquidity, "wrong total liquidity");
+        assertEq(rmm.totalLiquidity(), preTotalLiquidity + deltaLiquidity);
     }
 
     function test_allocate_AdjustsPool() public initDefaultPool {
+        deal(address(SY), address(this), 1_000 ether);
+
+        uint256 preReserveX = rmm.reserveX();
+        uint256 preReserveY = rmm.reserveY();
+
         (uint256 deltaXWad, uint256 deltaYWad,,) =
             rmm.prepareAllocate(0.1 ether, 0.1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
         rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
+
+        assertEq(rmm.reserveX(), preReserveX + deltaXWad);
+        assertEq(rmm.reserveY(), preReserveY + deltaYWad);
+        assertEq(rmm.lastTimestamp(), block.timestamp);
     }
 
     function test_allocate_TransfersTokens() public {
