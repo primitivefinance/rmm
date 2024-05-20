2036a36d1127798c3b629fa28e5c2e3f9076d178
clemlak <clementlakhal@gmail.com>
2024-05-20 12:13:04 +0200

    test: add test_swapX_TransfersTokens

--- test/SwapX.t.sol
+++ test/SwapX.t.sol
@@ -23,7 +23,28 @@ contract SwapXTest is SetUp {
         assertEq(rmm.reserveY(), preReserveY - amountOut);
     }
 
-    function test_swapX_TransfersTokens() public {}
+    function test_swapX_TransfersTokens() public initDefaultPool dealSY(address(this), 1_000 ether) {
+        (uint256 deltaXWad, uint256 deltaYWad,,) =
+            rmm.prepareAllocate(1 ether, 1 ether, PYIndex.wrap(YT.pyIndexCurrent()));
+        rmm.allocate(deltaXWad, deltaYWad, 0, address(this));
+
+        PYIndex index = PYIndex.wrap(YT.pyIndexCurrent());
+        uint256 amountIn = 1 ether;
+        (,, uint256 minAmountOut,,) = rmm.prepareSwap(address(SY), address(PT), amountIn, block.timestamp, index);
+
+        uint256 preRMMBalanceX = SY.balanceOf(address(rmm));
+        uint256 preRMMBalanceY = PT.balanceOf(address(rmm));
+        uint256 preBalanceX = SY.balanceOf(address(this));
+        uint256 preBalanceY = PT.balanceOf(address(this));
+
+        (uint256 amountOut,) = rmm.swapX(amountIn, minAmountOut, address(this), "");
+
+        assertEq(preBalanceX - amountIn, SY.balanceOf(address(this)));
+        assertEq(preBalanceY + amountOut, PT.balanceOf(address(this)));
+        assertEq(preRMMBalanceX + amountIn, SY.balanceOf(address(rmm)));
+        assertEq(preRMMBalanceY - amountOut, PT.balanceOf(address(rmm)));
+    }
+
     function test_swapX_EmitsSwap() public {}
     function test_swapX_RevertsWhenInsufficientOutput() public {}
 }
