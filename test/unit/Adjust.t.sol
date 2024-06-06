1fe7de47e24fa55110be139b04f1845c9c69dfbc
clemlak <clementlakhal@gmail.com>
2024-06-06 15:43:11 +0400

    test: update adjust test

--- test/unit/Adjust.t.sol
+++ test/unit/Adjust.t.sol
@@ -2,7 +2,19 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import {SetUp} from "../SetUp.sol";
-import {MockRMM} from "../MockRMM.sol";
+import {MockRMM, PYIndex} from "../MockRMM.sol";
 
-contract AdjustTest is Test {}
+contract AdjustTest is Test {
+    MockRMM rmm;
+
+    function setUp() public {
+        rmm = new MockRMM(address(0), "", "");
+    }
+
+    function test_adjust() public {
+        int256 deltaX;
+        int256 deltaY;
+        int256 deltaLiquidity;
+        rmm.adjust(deltaX, deltaY, deltaLiquidity, 0, PYIndex.wrap(0));
+    }
+}
