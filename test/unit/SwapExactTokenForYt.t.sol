cd81dc1e6cdd267de8215b8b7cb0e6487ac77147
clemlak <clementlakhal@gmail.com>
2024-06-07 15:28:17 +0400

    test: add test_swapExactTokenForYt_RevertsIfInvalidTokenIn

--- test/unit/SwapExactTokenForYt.t.sol
+++ test/unit/SwapExactTokenForYt.t.sol
@@ -1,10 +1,16 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {InvalidTokenIn} from "../../src/lib/RmmErrors.sol";
 import {SetUp, RMM} from "../SetUp.sol";
 
 contract SwapExactTokenForYtTest is SetUp {
     function test_swapExactTokenForYt_SwapsWETH() public initDefaultPool {
         rmm.swapExactTokenForYt(address(weth), 1 ether, 0, 0, address(this));
+    }
+
+    function test_swapExactTokenForYt_RevertsIfInvalidTokenIn() public initDefaultPool {
+        vm.expectRevert(abi.encodeWithSelector(InvalidTokenIn.selector, address(0xbeef)));
+        rmm.swapExactTokenForYt(address(0xbeef), 1 ether, 0, 0, address(this));
     }
 }
