ef6ec2b82aae919a0d8df2f5e80c9ae1b1233722
clemlak <clementlakhal@gmail.com>
2024-06-07 16:41:04 +0400

    test: add test_swapExactPtForSy_works

--- test/unit/SwapExactPtForSy.t.sol
+++ test/unit/SwapExactPtForSy.t.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.13;
 
 import {SetUp} from "../SetUp.sol";
 
-contract SwapExactPtForSyTest is SetUp {}
+contract SwapExactPtForSyTest is SetUp {
+    function test_swapExactPtForSy_works() public initDefaultPool {
+        rmm.swapExactPtForSy(1 ether, 0, address(this));
+    }
+}
