fb81f08b4268d9d14f5508a91ee3f34b52bfc67d
clemlak <clementlakhal@gmail.com>
2024-06-07 17:45:41 +0400

    test: add test_swapExactPtForSy_TransfersTokens

--- test/unit/SwapExactPtForSy.t.sol
+++ test/unit/SwapExactPtForSy.t.sol
@@ -1,10 +1,29 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {InsufficientOutput} from "../../src/lib/RmmErrors.sol";
 import {SetUp} from "../SetUp.sol";
 
 contract SwapExactPtForSyTest is SetUp {
-    function test_swapExactPtForSy_works() public initDefaultPool {
-        rmm.swapExactPtForSy(1 ether, 0, address(this));
+    function test_swapExactPtForSy_TransfersTokens() public initDefaultPool {
+        uint256 preSYBalance = ERC20(address(SY)).balanceOf(address(this));
+        uint256 prePTBalance = ERC20(address(PT)).balanceOf(address(this));
+
+        uint256 preSYBalanceRMM = ERC20(address(SY)).balanceOf(address(rmm));
+        uint256 prePTBalanceRMM = ERC20(address(PT)).balanceOf(address(rmm));
+
+        uint256 amountIn = 1 ether;
+        (uint256 amountOut,) = rmm.swapExactPtForSy(amountIn, 0, address(this));
+
+        assertEq(ERC20(address(SY)).balanceOf(address(this)), preSYBalance + amountOut);
+        assertEq(ERC20(address(PT)).balanceOf(address(this)), prePTBalance - amountIn);
+        assertEq(ERC20(address(SY)).balanceOf(address(rmm)), preSYBalanceRMM - amountOut);
+        assertEq(ERC20(address(PT)).balanceOf(address(rmm)), prePTBalanceRMM + amountIn);
+    }
+
+    function test_swapExactPtForSy_RevertsIfInsufficientOutput() public initDefaultPool {
+        vm.expectRevert(abi.encodeWithSelector(InsufficientOutput.selector));
+        rmm.swapExactPtForSy(1 ether, 1 ether, address(this));
     }
 }
