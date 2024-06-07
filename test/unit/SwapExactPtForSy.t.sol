4fa2573527f4dfe99e7b43bd0df5ae9d67a19ce1
clemlak <clementlakhal@gmail.com>
2024-06-07 18:22:48 +0400

    test: add fuzzing to test_swapExactPtForSy_TransfersTokens

--- test/unit/SwapExactPtForSy.t.sol
+++ test/unit/SwapExactPtForSy.t.sol
@@ -6,17 +6,17 @@ import {InsufficientOutput} from "../../src/lib/RmmErrors.sol";
 import {SetUp} from "../SetUp.sol";
 
 contract SwapExactPtForSyTest is SetUp {
-    function test_swapExactPtForSy_TransfersTokens() public initDefaultPool {
-        uint256 preSYBalance = ERC20(address(SY)).balanceOf(address(this));
+    function test_swapExactPtForSy_TransfersTokens(address to) public initDefaultPool {
+        uint256 preSYBalance = ERC20(address(SY)).balanceOf(to);
         uint256 prePTBalance = ERC20(address(PT)).balanceOf(address(this));
 
         uint256 preSYBalanceRMM = ERC20(address(SY)).balanceOf(address(rmm));
         uint256 prePTBalanceRMM = ERC20(address(PT)).balanceOf(address(rmm));
 
         uint256 amountIn = 1 ether;
-        (uint256 amountOut,) = rmm.swapExactPtForSy(amountIn, 0, address(this));
+        (uint256 amountOut,) = rmm.swapExactPtForSy(amountIn, 0, address(to));
 
-        assertEq(ERC20(address(SY)).balanceOf(address(this)), preSYBalance + amountOut);
+        assertEq(ERC20(address(SY)).balanceOf(address(to)), preSYBalance + amountOut);
         assertEq(ERC20(address(PT)).balanceOf(address(this)), prePTBalance - amountIn);
         assertEq(ERC20(address(SY)).balanceOf(address(rmm)), preSYBalanceRMM - amountOut);
         assertEq(ERC20(address(PT)).balanceOf(address(rmm)), prePTBalanceRMM + amountIn);
