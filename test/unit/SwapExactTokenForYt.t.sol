31c53584a88a1beb180fe88a1b5d824c2d7165a1
clemlak <clementlakhal@gmail.com>
2024-06-14 18:52:56 +0400

    test: almost fix test_swapExactTokenForYt_SwapsWETH

--- test/unit/SwapExactTokenForYt.t.sol
+++ test/unit/SwapExactTokenForYt.t.sol
@@ -1,6 +1,7 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {console} from "forge-std/Test.sol";
 import {IPYieldToken, PYIndexLib, PYIndex} from "pendle/core/StandardizedYield/PYIndex.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {InvalidTokenIn} from "../../src/lib/RmmErrors.sol";
@@ -10,13 +11,14 @@ contract SwapExactTokenForYtTest is SetUp {
     using PYIndexLib for IPYieldToken;
     using FixedPointMathLib for uint256;
 
-    function test_swapExactTokenForYt_SwapsWETH() public useDefaultPool {
+    function test_swapExactTokenForYt_SwapsWETH() public useSYPool withWETH(address(this), 1 ether) {
         uint256 amountIn = 1 ether;
         PYIndex index = YT.newIndex();
         (uint256 syMinted, uint256 ytOut) =
             rmm.computeTokenToYT(index, address(weth), amountIn, 500 ether, block.timestamp, 0, 1_000);
+        console.log("syMinted", syMinted);
         rmm.swapExactTokenForYt(
-            address(weth), 0, ytOut, syMinted, ytOut.mulDivDown(99, 100), 500 ether, 0.005 ether, address(this)
+            address(weth), amountIn, ytOut, syMinted, ytOut.mulDivDown(99, 100), 500 ether, 0.005 ether, address(this)
         );
     }
 
