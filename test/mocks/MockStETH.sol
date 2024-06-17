df86c862dfb7990dc99b3c0e9be9380425ce98f3
clemlak <clementlakhal@gmail.com>
2024-06-17 15:43:10 +0400

    test: add missing functions in MockStETH

--- test/mocks/MockStETH.sol
+++ test/mocks/MockStETH.sol
@@ -5,6 +5,8 @@ import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {IStETH} from "pendle/interfaces/IStETH.sol";
 
 contract MockStETH is MockERC20, IStETH {
+    uint256 public total;
+
     constructor() MockERC20("stETH", "stETH", 18) {}
 
     function getSharesByPooledEth(uint256 _ethAmount) external view returns (uint256) {
@@ -15,13 +17,25 @@ contract MockStETH is MockERC20, IStETH {
         return _sharesAmount;
     }
 
-    function submit(address referral) external payable returns (uint256 amount) {}
+    function submit(address referral) external payable returns (uint256 amount) {
+        mint(msg.sender, msg.value);
+        total += msg.value;
+        return msg.value;
+    }
 
-    function burnShares(address _account, uint256 _sharesAmount) external returns (uint256 newTotalShares) {}
+    function burnShares(address _account, uint256 _sharesAmount) external returns (uint256 newTotalShares) {
+        revert("not implemented");
+    }
 
-    function sharesOf(address account) external view returns (uint256) {}
+    function sharesOf(address account) external view returns (uint256) {
+        revert("not implemented");
+    }
 
-    function getTotalShares() external view returns (uint256) {}
+    function getTotalShares() external view returns (uint256) {
+        return total;
+    }
 
-    function getTotalPooledEther() external view returns (uint256) {}
+    function getTotalPooledEther() external view returns (uint256) {
+        return total;
+    }
 }
