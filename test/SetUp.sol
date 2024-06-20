824944af838d8f162cd52404b6b4078513065607
clemlak <clementlakhal@gmail.com>
2024-06-20 17:59:32 +0400

    test: update Forge Test import in test SetUp

--- test/SetUp.sol
+++ test/SetUp.sol
@@ -1,7 +1,7 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {WETH} from "solmate/tokens/WETH.sol";
 import {IPPrincipalToken} from "pendle/interfaces/IPPrincipalToken.sol";
 import {IPYieldToken} from "pendle/interfaces/IPYieldToken.sol";
