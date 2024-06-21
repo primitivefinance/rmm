4c5fa60f1ada355987cec26e97d845140f9d36d5
clemlak <clementlakhal@gmail.com>
2024-06-21 15:42:06 +0400

    chore: remove unused imports in SwapExactTokenForYtTest

--- test/unit/SwapExactTokenForYt.t.sol
+++ test/unit/SwapExactTokenForYt.t.sol
@@ -1,12 +1,10 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {console} from "forge-std/Test.sol";
 import {IPYieldToken, PYIndexLib, PYIndex} from "pendle/core/StandardizedYield/PYIndex.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {InvalidTokenIn} from "../../src/lib/RmmErrors.sol";
 import {SetUp, RMM} from "../SetUp.sol";
-import "forge-std/console2.sol";
 
 contract SwapExactTokenForYtTest is SetUp {
     using PYIndexLib for IPYieldToken;
