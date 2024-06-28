e87219091374543ffdcdaebc1946f87a6ee5e528
clemlak <clementlakhal@gmail.com>
2024-06-28 14:44:19 +0400

    chore: rename ApproxSpotPriceTest contract

--- test/unit/ApproxSpotPrice.t.sol
+++ test/unit/ApproxSpotPrice.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {SetUp, RMM} from "../SetUp.sol";
 
-contract ApproxSpotPrice is SetUp {
+contract ApproxSpotPriceTest is SetUp {
     function test_approxSpotPrice_IncreasesOverTime() public useDefaultPool {
         uint256 preSpotPrice = rmm.approxSpotPrice(syToAsset(rmm.reserveX()));
         rmm.setLastTimestamp(block.timestamp + 10 days);
