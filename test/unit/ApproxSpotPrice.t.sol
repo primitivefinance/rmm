1dffe1951754992f3883d67fd2f6c30c852078e5
clemlak <clementlakhal@gmail.com>
2024-06-28 15:03:35 +0400

    chore: remove unused import

--- test/unit/ApproxSpotPrice.t.sol
+++ test/unit/ApproxSpotPrice.t.sol
@@ -1,7 +1,7 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {SetUp, RMM} from "../SetUp.sol";
+import {SetUp} from "../SetUp.sol";
 
 contract ApproxSpotPriceTest is SetUp {
     function test_approxSpotPrice_IncreasesOverTime() public useDefaultPool {
