a17d30a1149384dc4e4288ecd0547461bcc78014
clemlak <clementlakhal@gmail.com>
2024-07-12 15:43:28 +0400

    test: add missing ETH deal in test_mintSY_MintsSYUsingETH

--- test/unit/MintSY.t.sol
+++ test/unit/MintSY.t.sol
@@ -5,6 +5,7 @@ import {SetUp} from "../SetUp.sol";
 
 contract MintSYTest is SetUp {
     function test_mintSY_MintsSYUsingETH() public useDefaultPool {
+        deal(address(this), 1 ether);
         rmm.mintSY{value: 1 ether}(address(0xbeef), address(0), 1 ether, 0);
         assertEq(SY.balanceOf(address(0xbeef)), 1 ether);
     }
